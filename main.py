18dda0266efc833ae7d293132b7c18d2e4c4e269
mrherres <t.r.heeres@student.rug.nl>
2022-05-05 21:22:36 +0200

    Added a tokenizer function that works with the support of spacy. More code is needed to complete the function, however.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import spacy
+from spacy.lang.nl.examples import sentences
+
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from os import listdir
 from retrieve_xml import retrieve_xml
 
 
-def webscraper():
-    url = "https://www.gutenberg.org/files/42263/42263-0.txt"
+def webscraper(url):
     page = urlopen(url)
     html = page.read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
@@ -43,14 +45,30 @@ def write_article(articles, wd):
     print("finished writing")
 
 
+def text_tokenizer():
+    with open("E:\\PycharmProjects\\Thesis\\passages\\newspaper\\1770_11.txt") as text:
+        nlp = spacy.load("nl_core_news_sm")
+        for word in text:
+            doc = nlp(word)
+            for i in doc:
+                print(i.text, i.lemma)
+
+
+
+
 
 def main():
-    # webscraper()
+    print("!")
+    # webscraper(url)
     # split()
+    # --------------------------------------------------------------------------
     # Change the working directory accordingly to the files you want to convert
-    wd = "E:\\Downloads\\1820"
-    articles = retrieve_xml(wd)
-    write_article(articles, wd)
+    # wd = "E:\\Downloads\\1820"
+    # articles = retrieve_xml(wd)
+    # write_article(articles, wd)
+    # --------------------------------------------------------------------------
+    # Code for lemmatization etc.
+    # text_tokenizer()
 
 
 if __name__ == "__main__":
